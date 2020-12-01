ba53d4644c38840fc6df90f849724ad94e2bc0de
kinopi <constellation.2772@gmail.com>
2020-12-01 16:29:45 +0900

    成績フィルタリング完成

--- prottypesys/GradeInquiry/teacher/urls.py
+++ prottypesys/GradeInquiry/teacher/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from . import views
-from .views import AllGradeShowViewSet
+from .views import AllGradeShowViewSet,AGradeShowViewSet,BGradeShowViewSet,CGradeShowViewSet,DGradeShowViewSet
 
 urlpatterns = [
     # path('', views.file_upload, name='teacher'),
@@ -10,6 +10,9 @@ urlpatterns = [
     path('Grade/', views.file_upload_Grade, name='Grade'),
     path('Student/', views.file_upload_Student, name='Student'),
     path('Subject/', views.file_upload_Subject, name='Subject'),
-    path('allgrade/',AllGradeShowViewSet.as_view(),name='allgrade')
-
+    path('allgrade/',AllGradeShowViewSet.as_view(),name='allgrade'),
+    path('agrade/',AGradeShowViewSet.as_view(),name='agrade'),
+    path('bgrade/', BGradeShowViewSet.as_view(), name='bgrade'),
+    path('cgrade/', CGradeShowViewSet.as_view(), name='cgrade'),
+    path('dgrade/', DGradeShowViewSet.as_view(), name='dgrade'),
 ]
--- prottypesys/GradeInquiry/views.py
+++ prottypesys/GradeInquiry/views.py
@@ -13,11 +13,6 @@ from .serializers import Userserializers, Gradeserializers,Loginserializers
 class CreateUserView(generics.CreateAPIView):
     serializer_class = Userserializers
     permission_classes = (AllowAny,)
-
-
-
-
-
 class LoginView(generics.ListCreateAPIView):
 
     serializer_class = Loginserializers
